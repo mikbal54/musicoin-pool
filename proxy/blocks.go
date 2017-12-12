2312d8dc605d639644ea2226ae522644df771f23
mikbal54 <34392771+mikbal54@users.noreply.github.com>
2017-12-12 15:49:45 +0300

    Update blocks.go

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/pedrogms82/musicoin-pool/rpc"
-	"github.com/pedrogms82/musicoin-pool/util"
+	"github.com/mikbal54/musicoin-pool/rpc"
+	"github.com/mikbal54/musicoin-pool/util"
 )
 
 const maxBacklog = 3
