5e1c24d9ee6f77b908e6209a3a3557b2eab8bab8
mikbal54 <34392771+mikbal54@users.noreply.github.com>
2017-12-12 15:47:57 +0300

    Update unlocker.go

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -10,9 +10,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/math"
 
-    "github.com/pedrogms82/musicoin-pool/rpc"
-    "github.com/pedrogms82/musicoin-pool/storage"
-    "github.com/pedrogms82/musicoin-pool/util"
+    "github.com/mikbal54/musicoin-pool/rpc"
+    "github.com/mikbal54/musicoin-pool/storage"
+    "github.com/mikbal54/musicoin-pool/util"
 )
 
 type UnlockerConfig struct {
